03f2808da86121e9ab724e803a20d1a5f1871de2
CSE-49 Aryan-Shukla <1032220655@tcetmumbai.in>
2025-07-31 20:08:34 +0000

    403. That’s an error.
    We're sorry, but you do not have access to this p

--- src/app/api/auth/callback/google/route.ts
+++ src/app/api/auth/callback/google/route.ts
@@ -1,15 +1,22 @@
 
 import { handleAuthorizationCode } from '@/lib/auth';
-import { type NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams
     const code = searchParams.get('code')
+    const error = searchParams.get('error');
+
+    if (error) {
+        console.error('Error during Google OAuth:', error);
+        return NextResponse.redirect(new URL('/?error=auth_failed', request.url));
+    }
 
     if (typeof code === 'string') {
         await handleAuthorizationCode(code);
+        return NextResponse.redirect(new URL('/', request.url));
     } else {
-        // Handle error, maybe redirect to an error page
         console.error('No authorization code provided.');
+        return NextResponse.redirect(new URL('/?error=no_code', request.url));
     }
 }
